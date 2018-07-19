f7a3e64880011419e88d30d173479cbe5380d163
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 17:06:25 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
   arr = str.split(" ")
   new_str = ""
-  arr.each {|word| new_str << word.reverse}
+  arr.each {|word| new_str << word.reverse + " "}
   new_str
 end
   
